02b8ad60b432f70062490e465434fa71ec465744
Angel Rodriguez <angelr1076@gmail.com>
2021-12-30 19:08:07 -0500

    first commit

---
+++ practice_exercises/README.md
@@ -0,0 +1,2 @@
+Blockchain-Developer-Nanodegree
+Repository for creation of my own private blockchain, and secure a digital asset using blockchain identity. Explore the Ethereum platform, and use Solidity and smart contracts to develop your own decentralized app.
---
+++ practice_exercises/blocks/block.js
@@ -0,0 +1,39 @@
+/**
+ * Import crypto-js/SHA256 library
+ */
+const SHA256 = require('crypto-js/sha256');
+
+/**
+ * Class with a constructor for block 
+ */
+class Block {
+
+	constructor(data){
+		this.id = 0;
+        this.nonce = 144444;
+      	this.body = data;
+      	this.hash = "";
+    }
+    
+    /**
+     * Step 1. Implement `generateHash()`
+     * method that return the `self` block with the hash.
+     * 
+     * Create a Promise that resolve with `self` after you create 
+     * the hash of the object and assigned to the hash property `self.hash = ...`
+     */
+  	// 
+  	generateHash() {
+      	// Use this to create a temporary reference of the class object
+      	let self = this;
+        var promise = new Promise(function(resolve, reject) {
+          // do a thing, possibly async, then…
+          self.hash = SHA256(JSON.stringify(self)).toString();
+		  resolve(self);
+        });
+      return promise;
+    }
+}
+
+// Exporting the class Block to be reuse in other files
+module.exports.Block = Block;
