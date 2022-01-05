98136de52af189ad92aafabac26c9eb64c6c61bd
Angel Rodriguez <angelr1076@gmail.com>
2022-01-05 18:18:35 -0500

    Added EncodingDecoding folder

--- practice_exercises/Project_2/simpleChain.js
+++ practice_exercises/Project_2/simpleChain.js
@@ -9,13 +9,13 @@ const SHA256 = require('crypto-js/sha256');
 |  ====================================================*/
 
 class Block {
-    constructor(data) {
-        this.height = '';
-        this.timeStamp = '';
-        this.data = data;
-        this.previousHash = '0x';
-        this.hash = '';
-    }
+  constructor(data) {
+    this.height = '';
+    this.timeStamp = '';
+    this.data = data;
+    this.previousHash = '0x';
+    this.hash = '';
+  }
 }
 
 /* ===== Blockchain ===================================
@@ -30,40 +30,40 @@ class Block {
 |  ====================================================*/
 
 class Blockchain {
-    constructor() {
-        // new chain array
-        this.chain = [];
-        // add first genesis block
-        this.addBlock(this.createGenesisBlock());
-    }
+  constructor() {
+    // new chain array
+    this.chain = [];
+    // add first genesis block
+    this.addBlock(this.createGenesisBlock());
+  }
 
-    createGenesisBlock() {
-        return new Block('First block in the chain - Genesis block');
-    }
+  createGenesisBlock() {
+    return new Block('First block in the chain - Genesis block');
+  }
 
-    // getLatest block method
-    getLatestBlock() {
-        // Go to the end of the chain index minus 1
-        return this.chain[this.chain.length - 1];
-    }
+  // getLatest block method
+  getLatestBlock() {
+    // Go to the end of the chain index minus 1
+    return this.chain[this.chain.length - 1];
+  }
 
-    // addBlock method
-    addBlock(newBlock) {
-        // block height
-        newBlock.height = this.chain.length;
-        // UTC timestamp
-        newBlock.timeStamp = new Date().getTime().toString().slice(0, -3);
-        console.log(newBlock.timeStamp);
-        if (this.chain.length > 0) {
-            // previous block hash
-            newBlock.previousHash = this.getLatestBlock().hash;
-        }
-        // SHA256 requires a string of data
-        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-        console.log(JSON.stringify(newBlock));
-        // add block to chain
-        this.chain.push(newBlock);
+  // addBlock method
+  addBlock(newBlock) {
+    // block height
+    newBlock.height = this.chain.length;
+    // UTC timestamp
+    newBlock.timeStamp = new Date().getTime().toString().slice(0, -3);
+    console.log(newBlock.timeStamp);
+    if (this.chain.length > 0) {
+      // previous block hash
+      newBlock.previousHash = this.getLatestBlock().hash;
     }
+    // SHA256 requires a string of data
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+    console.log(JSON.stringify(newBlock));
+    // add block to chain
+    this.chain.push(newBlock);
+  }
 }
 
 let blockchain = new Blockchain();
