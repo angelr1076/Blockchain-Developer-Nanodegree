18cfac5c45579ebf1e65f4e21ea965e78b9da9f7
Angel Rodriguez <angelr1076@gmail.com>
2022-01-12 15:52:16 -0500

    completed addBlock method

--- main_projects/BuildABlockchain/src/block.js
+++ main_projects/BuildABlockchain/src/block.js
@@ -13,76 +13,76 @@ const SHA256 = require('crypto-js/sha256');
 const hex2ascii = require('hex2ascii');
 
 class Block {
-  // Constructor - argument data will be the object containing the transaction data
-  constructor(data) {
-    this.hash = null; // Hash of the block
-    this.height = 0; // Block Height (consecutive number of each block)
-    this.body = Buffer.from(JSON.stringify(data)).toString('hex'); // Will contain the transactions stored in the block, by default it will encode the data
-    this.time = 0; // Timestamp for the Block creation
-    this.previousBlockHash = null; // Reference to the previous Block Hash
-  }
+    // Constructor - argument data will be the object containing the transaction data
+    constructor(data) {
+        this.hash = null; // Hash of the block
+        this.height = 0; // Block Height (consecutive number of each block)
+        this.body = Buffer.from(JSON.stringify(data)).toString('hex'); // Will contain the transactions stored in the block, by default it will encode the data
+        this.time = 0; // Timestamp for the Block creation
+        this.previousBlockHash = null; // Reference to the previous Block Hash
+    }
 
-  /**
-   *  validate() method will validate if the block has been tampered or not.
-   *  Been tampered means that someone from outside the application tried to change
-   *  values in the block data as a consecuence the hash of the block should be different.
-   *  Steps:
-   *  1. Return a new promise to allow the method be called asynchronous.
-   *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
-   *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
-   *  4. Compare if the auxiliary hash value is different from the calculated one.
-   *  5. Resolve true or false depending if it is valid or not.
-   *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
-   */
+    /**
+     *  validate() method will validate if the block has been tampered or not.
+     *  Been tampered means that someone from outside the application tried to change
+     *  values in the block data as a consecuence the hash of the block should be different.
+     *  Steps:
+     *  1. Return a new promise to allow the method be called asynchronous.
+     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
+     *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
+     *  4. Compare if the auxiliary hash value is different from the calculated one.
+     *  5. Resolve true or false depending if it is valid or not.
+     *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
+     */
 
-  validate() {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      // Save in auxiliary variable the current block hash
-      let currentHash = { ...self, hash: null };
+    validate() {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            // Save in auxiliary variable the current block hash
+            let currentHash = {...self, hash: null };
 
-      console.log('Current hash: ', currentHash);
-      // Recalculate the hash of the Block
-      const recalculatedHash = SHA256(JSON.stringify(currentHash)).toString();
-      console.log('Recalc hash: ', recalculatedHash);
-      // Comparing if the hashes changed
-      self.hash === recalculatedHash
-        ? resolve(true)
-        : resolve(
-            false,
-            Error(
-              `The current block has been tampered with ${currentHash} !== ${recalculatedHash}`,
-            ),
-          );
-    });
-  }
+            // console.log('Current hash: ', currentHash);
+            // Recalculate the hash of the Block
+            const recalculatedHash = SHA256(JSON.stringify(currentHash)).toString();
+            // console.log('Recalc hash: ', recalculatedHash);
+            // Comparing if the hashes changed
+            self.hash === recalculatedHash ?
+                resolve(true) :
+                resolve(
+                    false,
+                    Error(
+                        `The current block has been tampered with ${currentHash} !== ${recalculatedHash}`
+                    )
+                );
+        });
+    }
 
-  /**
-   *  Auxiliary Method to return the block body (decoding the data)
-   *  Steps:
-   *
-   *  1. Use hex2ascii module to decode the data
-   *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
-   *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
-   *     or Reject with an error.
-   */
-  getBData() {
-    // Getting the encoded data saved in the Block
-    let self = this;
-    // Decoding the data to retrieve the JSON representation of the object
-    const decodedData = hex2ascii(self.body);
-    // Parse the data to an object to be retrieve.
-    const block = JSON.parse(decodedData);
+    /**
+     *  Auxiliary Method to return the block body (decoding the data)
+     *  Steps:
+     *
+     *  1. Use hex2ascii module to decode the data
+     *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
+     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
+     *     or Reject with an error.
+     */
+    getBData() {
+        // Getting the encoded data saved in the Block
+        let self = this;
+        // Decoding the data to retrieve the JSON representation of the object
+        const decodedData = hex2ascii(self.body);
+        // Parse the data to an object to be retrieve.
+        const block = JSON.parse(decodedData);
 
-    return new Promise((resolve, reject) => {
-      // If previousHash is null, we are on the genesis block
-      self.previousBlockHash === null ? reject(Error, 'Genesis block.') : false;
-      // Resolve with the data if the object isn't the Genesis block
-      block === true
-        ? resolve(block)
-        : reject(Error('Failed decoding the current block.'));
-    });
-  }
+        return new Promise((resolve, reject) => {
+            // If previousHash is null, we are on the genesis block
+            self.previousBlockHash === null ? reject(Error, 'Genesis block.') : false;
+            // Resolve with the data if the object isn't the Genesis block
+            block === true ?
+                resolve(block) :
+                reject(Error('Failed decoding the current block.'));
+        });
+    }
 }
 
 module.exports.Block = Block; // Exposing the Block class as a module
