81f8d853b7e0775d5377b9dc371b9c4d4b28a9a3
Angel Rodriguez <angelr1076@gmail.com>
2022-01-23 11:40:07 -0500

    Updated index.html script code

--- Practice/Web3/index.js
+++ Practice/Web3/index.js
@@ -1,9 +1,14 @@
 // -- Step 1: Set up the appropriate configuration
 let Web3 = require('web3');
-let web3 = new Web3('<infura mainnet address>');
+// let web3 = new Web3('<infura mainnet address>');
+let web3 = new Web3('http://127.0.0:8545');
 let gasPrice = web3.eth.getGasPrice().then(console.log());
 let uncle = web3.eth.getUncle(500, 0).then(console.log);
-let transCount = web3.eth.getBlockTransactionCount('pending').then(console.log);
-console.log(gasPrice);
-console.log(uncle);
+// let transCount = web3.eth.getBlockTransactionCount('pending').then(console.log);
+// console.log(gasPrice);
+// console.log(uncle);
+// console.log(transCount);
+let transCount = web3.eth
+  .getTransactionCount('0x1E07E1eC1559138e03EB5F0C89BEAF9bD5AB3332')
+  .then(console.log);
 console.log(transCount);
