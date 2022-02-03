3d8719f428b2654777770fd84e05d3830b65b9e4
Angel Rodriguez <angelr1076@gmail.com>
2022-02-03 07:49:37 -0500

    Updated ERC-20 SampleToken

--- Practice/ERC-20SampleToken/contracts/SampleToken.sol
+++ Practice/ERC-20SampleToken/contracts/SampleToken.sol
@@ -1,13 +1,10 @@
-pragma solidity >=0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract SampleToken is ERC20Detailed, ERC20 {
-    
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _initialSupply) 
-    ERC20Detailed(_name, _symbol, _decimals) public {
-        require(_initialSupply > 0, "INITIAL_SUPPLY has to be greater than 0");
-        _mint(msg.sender, _initialSupply);
+contract SampleToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        _mint(msg.sender, initialSupply);
     }
 }
