ce505f569077884504da82079372cd86f7fcd894
Angel Rodriguez <angelr1076@gmail.com>
2022-02-08 08:10:20 -0500

    installed openzeppelin 2.4

--- PracticeProjects/starNotaryv2/contracts/StarNotary.sol
+++ PracticeProjects/starNotaryv2/contracts/StarNotary.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
 import "../app/node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
@@ -36,7 +35,7 @@ contract StarNotary is ERC721 {
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
         require(msg.value > starCost, "You need to have enough Ether");
-        transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
+        _transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
         address payable ownerAddressPayable = _make_payable(ownerAddress); // We need to make this conversion to be able to use transfer() function to transfer ethers
         ownerAddressPayable.transfer(starCost);
         if(msg.value > starCost) {
