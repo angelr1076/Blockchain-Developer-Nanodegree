f1863a55c5e8351123147e01448b936750cdc88a
Angel Rodriguez <angelr1076@gmail.com>
2022-02-08 07:51:36 -0500

    Added StarNotary.sol contract

--- PracticeProjects/starNotaryv2/contracts/StarNotary.sol
+++ PracticeProjects/starNotaryv2/contracts/StarNotary.sol
@@ -1,9 +1,9 @@
-pragma solidity >=0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "../app/node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract StarNotary is ERC721 {
-
     struct Star {
         string name;
     }
@@ -11,12 +11,12 @@ contract StarNotary is ERC721 {
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
 
-    
+
     // Create Star using the Struct
-    function createStar(string memory _name, uint256 _tokenId) public {
-        Star memory newStar = Star(_name);
-        tokenIdToStarInfo[_tokenId] = newStar;
-        _mint(msg.sender, _tokenId);
+    function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
+        Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
+        tokenIdToStarInfo[_tokenId] = newStar; // Creating in memory the Star -> tokenId mapping
+        _mint(msg.sender, _tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
     }
 
     // Putting an Star for sale (Adding the star tokenid into the mapping starsForSale, first verify that the sender is the owner)
@@ -25,6 +25,8 @@ contract StarNotary is ERC721 {
         starsForSale[_tokenId] = _price;
     }
 
+
+    // Function that allows you to convert an address into a payable address
     function _make_payable(address x) internal pure returns (address payable) {
         return address(uint160(x));
     }
@@ -34,14 +36,13 @@ contract StarNotary is ERC721 {
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
         require(msg.value > starCost, "You need to have enough Ether");
-        _transferFrom(ownerAddress, msg.sender, _tokenId);
-        address payable ownerAddressPayable = _make_payable(ownerAddress);
+        transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
+        address payable ownerAddressPayable = _make_payable(ownerAddress); // We need to make this conversion to be able to use transfer() function to transfer ethers
         ownerAddressPayable.transfer(starCost);
         if(msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
     }
-
 }
 
 
