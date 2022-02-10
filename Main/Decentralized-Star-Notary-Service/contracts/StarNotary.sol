09466dad701bd30caa27d7f9b7865ed9d6c5cea1
Angel Rodriguez <angelr1076@gmail.com>
2022-02-10 08:15:37 -0500

    removed .github folder and added token name constructor to starnotary contract

--- Main/Decentralized-Star-Notary-Service/contracts/StarNotary.sol
+++ Main/Decentralized-Star-Notary-Service/contracts/StarNotary.sol
@@ -14,7 +14,7 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    constructor() public ERC721("GraceToken", "EGR") {}
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
