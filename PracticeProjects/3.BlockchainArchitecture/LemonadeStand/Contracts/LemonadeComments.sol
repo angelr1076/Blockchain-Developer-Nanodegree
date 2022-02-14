453a2e6d4f86ec0eed5a76bfaadc229b0080ffa1
Angel Rodriguez <angelr1076@gmail.com>
2022-02-14 17:26:23 -0500

    Added PracticeProject folders

---
+++ PracticeProjects/3.BlockchainArchitecture/LemonadeStand/Contracts/LemonadeComments.sol
@@ -0,0 +1,33 @@
+// Define a Supply Chain Contract "LemonadeStand"
+
+// Variable: 'Owner'
+
+// Variable: 'skuCount'
+
+// State: For Sale
+
+// Struct 'Item' with the following fields: name, sku, price, state, seller, buyer
+
+// Mapping: Assign 'Item' a SKU
+
+// Event ForSale
+
+// Event Sold
+
+// Modifier: Only Owner
+
+// Modifier: Verify Caller
+
+// Modifier: Paid Enough
+
+// Modifier: For Sale
+
+// Modifier: Sold
+
+// Function: Constructor to set some initial values
+
+// Function: Add Item
+
+// Function: Buy Item
+
+// Function: Fetch Item
