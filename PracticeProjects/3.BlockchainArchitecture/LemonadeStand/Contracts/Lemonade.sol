9863efad68229e719ffb2f9913b22cf1a42ed1c9
Angel Rodriguez <angelr1076@gmail.com>
2022-02-18 08:08:19 -0500

    Updated Lemonade contract to use solidity 0.8.12 and updated breakable functions

--- PracticeProjects/3.BlockchainArchitecture/LemonadeStand/Contracts/Lemonade.sol
+++ PracticeProjects/3.BlockchainArchitecture/LemonadeStand/Contracts/Lemonade.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.24;
+pragma solidity 0.8.12;
 
 // Define a contract 'Lemonade Stand'
 contract LemonadeStand {
@@ -19,10 +19,10 @@ contract LemonadeStand {
         uint  sku;
         uint  price;
         State  state;
-        address  seller;
-        address  buyer;
+        address seller;
+        address buyer;
     }
-
+  
     // Define a public mapping 'items' that maps the SKU (a number) to an Item.
     mapping (uint => Item) items;
 
@@ -66,23 +66,24 @@ contract LemonadeStand {
         _;
         uint _price = items[_sku].price;
         uint amountToRefund = msg.value - _price;
-        items[_sku].buyer.transfer(amountToRefund);
+      // Breaking changes https://docs.soliditylang.org/en/v0.8.2/080-breaking-changes.html#how-to-update-your-code
+      // Example https://ethereum.stackexchan e.com/questions/94707/how-to-fix-typeerror-type-address-is-not-implicitly-convertible-to-expected-ty
+        payable(items[_sku].buyer).transfer(amountToRefund);
     }
 
-    constructor() public payable {
+    constructor() payable {
         owner = msg.sender;
         skuCount = 0;
     }
 
-    function addItem(string _name, uint _price) onlyOwner public {
+    function addItem(string memory _name, uint _price) onlyOwner public {
         // Increment sku
         skuCount = skuCount + 1;
 
         // Emit the appropriate event
         emit ForSale(skuCount);
-
         // Add the new item into inventory and mark it for sale
-        items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: 0});
+        items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
     }
 
     function buyItem(uint sku) forSale(sku) paidEnough(items[sku].price) checkValue(sku) public payable {
@@ -96,7 +97,7 @@ contract LemonadeStand {
         items[sku].state = State.Sold;
 
         // Transfer money to seller
-        items[sku].seller.transfer(price);
+        payable(items[sku].seller).transfer(price);
 
         // Emit the appropriate event
         emit Sold(sku);
@@ -110,7 +111,7 @@ contract LemonadeStand {
         emit Shipped(sku);
     }
 
-    function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, string stateIs, address seller, address buyer) {
+    function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, string memory stateIs, address seller, address buyer) {
         uint state;
         name = items[_sku].name;
         sku = items[_sku].sku;
